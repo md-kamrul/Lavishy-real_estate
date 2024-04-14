e26afd5c44167a64ddcf3cfd3e037efe0e8b6a3a
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-04-14 21:50:42 +0600

    card design done

--- src/components/Home/DataCard.jsx
+++ src/components/Home/DataCard.jsx
@@ -1,17 +1,36 @@
+import { AiFillDollarCircle } from "react-icons/ai";
+import { GrStatusInfo } from "react-icons/gr";
+import { FaLocationDot } from "react-icons/fa6";
+
 const dataCard = ({ singleData }) => {
 
-    const { estate_title, image, segment_name, description, price, status, area, location, facilities } = singleData;
+    const { estate_title, image, description, price, status, location } = singleData;
 
     return (
         <div>
-            <div className="card border border-[#00c867]">
+            <div className="card border border-[#2b3440]">
                 <figure><img src={image} alt="resorts" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{estate_title}</h2>
-                    <p>{description}</p>
-                    <p>{segment_name}</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                    {
+                        description.length > 100 ?
+                            <p>{description.slice(0, 100)}....</p>
+                            :
+                            <p>{description}</p>
+                    }
+                    <div className="my-5 flex gap-3">
+                        <div className="font-medium flex gap-1">
+                            <AiFillDollarCircle className="text-2xl text-[#00c867]" />{price}
+                        </div>
+                        <div className="font-medium flex gap-1">
+                            <GrStatusInfo className="text-2xl text-[#00c867]" />{status}
+                        </div>
+                    </div>
+                    <div className="flex gap-3">
+                        <FaLocationDot className="text-2xl text-[#00c867]" />{location}
+                    </div>
+                    <div className="w-[40%] mx-auto mt-2">
+                        <button className="w-full mx-auto bg-[#00c867] text-white rounded-lg py-3 px-2">View Details</button>
                     </div>
                 </div>
             </div>
